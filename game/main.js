abbfb2655124cde52f597d5d9a4a6fdfbc60de9c
Kai Jukarainen <kai.jukarainen@gmail.com>
2022-10-06 15:25:35 +0300

    new images, sound effect, styles, score counters.

--- game/main.js
+++ game/main.js
@@ -2,22 +2,30 @@
 let frames = 0;
 let score = 0;
 let i = 0;
+let full = 0;
+
+//Canvas//
 const canvas = document.getElementById('canvas');
 const contex = canvas.getContext('2d');
 const canvasW = canvas.width = 600;
 const canvasH = canvas.height = 600;
+
+//Image control//
 const img = new Image();
 img.src = '../sprites/blobsprite2.0.png';
+const img2 = new Image();
+img2.src = '../sprites/blobsprite3.0.png';
 const spriteW = 319.7;
 const spriteH = 320;
 let frame = 12;
 let gameFrame = 0;
 const staggerFrames = 8;
+
+//Click controll//
 let canvasElem = document.querySelector("canvas");
 canvasElem.addEventListener("mousedown", function(e){getMousePosition(canvasElem, e);});
-
-
 function getMousePosition(canvas, event) {
+    event.preventDefault();
     let rect = canvas.getBoundingClientRect();
     let x = event.clientX - rect.left;
     let y = event.clientY - rect.top;
@@ -29,7 +37,11 @@ function getMousePosition(canvas, event) {
 
 }
 
+//Audio//
+const audio = new Audio();
+audio.src = '../sounds/push.mp3';
 
+//Start animation//
 function animate(){
 
     contex.clearRect(0, 0, canvasW, canvasH);
@@ -47,6 +59,7 @@ function animate(){
 };
 animate();
 
+//Gameplay animation//
 function nextFrame(){
     contex.clearRect(0, 0, canvasW, canvasH);
     contex.drawImage(img, frames * spriteW, 0 * spriteH, spriteW, spriteH, 140, 280, spriteW, spriteH);
@@ -54,12 +67,15 @@ function nextFrame(){
         if (frame < 11) frame++;
         else frame = 0;
     }
+    if (score > 665) {
+        contex.drawImage(img2, frames * spriteW, 0 * spriteH, spriteW, spriteH, 140, 280, spriteW, spriteH);
+    }
     gameFrame++;
 };
 
-
-
+//Scores and gameplay//
 function add(){
+    audio.play();
     if (frames < 11){
         frames++;
     }else{
@@ -68,9 +84,14 @@ function add(){
     if (frames > 1 && i == 1) {
         score++;
     }
-    document.getElementById('count').innerHTML ='SCORE: ' + score;
+    if (frames == 0) {
+        document.getElementById('cb').innerHTML ='CYCLE: ' + full;
+        full++;
+    }
+    document.getElementById('sb').innerHTML ='SCORE: ' + score;
 }
 function gameOn() {
     document.getElementById('btn').style.display = "none";
     i = 1;
 }
+
